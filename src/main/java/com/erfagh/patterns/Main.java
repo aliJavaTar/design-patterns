6798a99b8d275df8f14e8387a90b9a084efd915d
A.erfagh <ali77erfagh99@gmail.com>
2022-12-04 16:21:20 +0330

    done

--- src/main/java/com/erfagh/patterns/Main.java
+++ src/main/java/com/erfagh/patterns/Main.java
@@ -1,7 +1,26 @@
 package com.erfagh.patterns;
 
+import com.erfagh.patterns.creational.abstractfactory.Application;
+import com.erfagh.patterns.creational.abstractfactory.GUIFactory;
+import com.erfagh.patterns.creational.abstractfactory.MacOSFactory;
+import com.erfagh.patterns.creational.abstractfactory.WindowsFactory;
+
 public class Main {
+    private static Application configureApplication() {
+        Application application;
+        GUIFactory factory;
+        String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.contains("mac"))
+            factory = new MacOSFactory();
+        else
+            factory = new WindowsFactory();
+
+        application = new Application(factory);
+        return application;
+    }
+
     public static void main(String[] args) {
-        System.out.println("hi fucker");
+        Application app = configureApplication();
+        app.paint();
     }
 }
--- src/main/java/com/erfagh/patterns/creational/abstractfactory/Application.java
+++ src/main/java/com/erfagh/patterns/creational/abstractfactory/Application.java
@@ -4,9 +4,9 @@ public class Application {
     private final Button button;
     private final Checkbox checkbox;
 
-    public Application(Button button, Checkbox checkbox) {
-        this.button = button;
-        this.checkbox = checkbox;
+    public Application(GUIFactory factory) {
+        this.button = factory.createButton();
+        this.checkbox = factory.createCheckbox();
     }
 
     public void paint() {
