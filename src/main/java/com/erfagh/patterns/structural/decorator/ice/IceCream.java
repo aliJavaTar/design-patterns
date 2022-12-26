6a097377d1169b739913d2026e582f626a9c3675
A.erfagh <ali77erfagh99@gmail.com>
2022-12-26 10:12:22 +0330

    add interface IceCream and add concrete class impl of interface

---
+++ src/main/java/com/erfagh/patterns/structural/decorator/ice/IceCream.java
@@ -0,0 +1,6 @@
+package com.erfagh.patterns.structural.decorator.ice;
+
+public interface IceCream {
+
+    String makeIceCream();
+}
---
+++ src/main/java/com/erfagh/patterns/structural/decorator/ice/SimpleIceCream.java
@@ -0,0 +1,8 @@
+package com.erfagh.patterns.structural.decorator.ice;
+
+public class SimpleIceCream implements IceCream{
+    @Override
+    public String makeIceCream() {
+        return "simple ice cream";
+    }
+}
