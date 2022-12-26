bb7a80a90dc102b41075833979e67da5cde1f569
A.erfagh <ali77erfagh99@gmail.com>
2022-12-26 10:16:58 +0330

    add ChocolateDecorator

---
+++ src/main/java/com/erfagh/patterns/structural/decorator/ice/ChocolateDecorator.java
@@ -0,0 +1,17 @@
+package com.erfagh.patterns.structural.decorator.ice;
+
+public class ChocolateDecorator extends IceCreamDecorator {
+
+    public ChocolateDecorator(IceCream iceCream) {
+        super(iceCream);
+    }
+
+    @Override
+    public String makeIceCream() {
+        return super.makeIceCream() + addChocolate();
+    }
+
+    private String addChocolate() {
+        return "add Chocolate";
+    }
+}
