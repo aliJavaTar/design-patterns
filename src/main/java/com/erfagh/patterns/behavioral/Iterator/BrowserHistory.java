44e95accc8dd59fb6457bd3dfed13bafe47bd1ec
A.erfagh <ali77erfagh99@gmail.com>
2022-12-28 13:21:29 +0330

    add method to array rather than list

--- src/main/java/com/erfagh/patterns/behavioral/Iterator/BrowserHistory.java
+++ src/main/java/com/erfagh/patterns/behavioral/Iterator/BrowserHistory.java
@@ -2,41 +2,64 @@ package com.erfagh.patterns.behavioral.Iterator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class BrowserHistory {
-    private List<String> urls = new ArrayList<>();
+    private String[] urls = new String[10];
+    int count;
 
     public void push(String url) {
-        urls.add(url);
+        urls[count++] = url;
     }
 
     public String pop() {
-        var lastIndex = urls.size() - 1;
-        var lastUrl = urls.get(lastIndex);
-        urls.remove(lastUrl);
-        return lastUrl;
-    }
-    public Iterator createIterator()
-    {
-        return new ListIterator(this);
+        return urls[count--];
     }
 
-    public class ListIterator implements Iterator {
-        private BrowserHistory history;
+    public Iterator createIterator() {
+        return new ArrayIterator(this);
+    }
+
+//    public static class ListIterator implements Iterator {
+//        private final BrowserHistory history;
+//        private int index;
+//
+//        public ListIterator(BrowserHistory history) {
+//            this.history = history;
+//        }
+//
+//        @Override
+//        public boolean hasNext() {
+//            return (index < history. );
+//        }
+//
+//        @Override
+//        public String current() {
+//            return history.urls.get(index);
+//        }
+//
+//        @Override
+//        public void next() {
+//            index++;
+//        }
+//    }
+
+    public static class ArrayIterator implements Iterator {
+        private final BrowserHistory history;
         private int index;
 
-        public ListIterator(BrowserHistory history) {
+        public ArrayIterator(BrowserHistory history) {
             this.history = history;
         }
 
         @Override
         public boolean hasNext() {
-            return (index < history.urls.size());
+            return (index < history.count);
         }
 
         @Override
         public String current() {
-            return history.urls.get(index);
+            return history.urls[index];
         }
 
         @Override
