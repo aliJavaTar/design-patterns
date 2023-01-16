71945c319dc0b9f4dab6bd2eed20b27dae92a9b2
A.erfagh <ali77erfagh99@gmail.com>
2023-01-16 13:57:33 +0330

    add MovieBuilder

--- src/main/java/com/erfagh/patterns/creational/builder/good/MovieBuilder.java
+++ src/main/java/com/erfagh/patterns/creational/builder/good/MovieBuilder.java
@@ -1,10 +1,18 @@
 package com.erfagh.patterns.creational.builder.good;
 
+import com.erfagh.patterns.creational.builder.bad.Movie;
 import com.erfagh.patterns.creational.builder.bad.Slide;
 
 public class MovieBuilder implements PresentationBuilder {
+
+    private final Movie movie;
+
+    public MovieBuilder(Movie movie) {
+        this.movie = movie;
+    }
+
     @Override
     public void addSlide(Slide slide) {
-
+      movie.addFrame(slide.text(),3);
     }
 }
